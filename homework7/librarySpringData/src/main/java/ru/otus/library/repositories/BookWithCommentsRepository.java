a2c8523f8bf6ff9110100a6b1aed768782c4ed6d
RomanKemaykin <rkemaykin@diasoft.ru>
2020-10-28 09:36:44 +0300

    удаляем из ветки прошлого ДЗ новое ДЗ

--- homework7/librarySpringData/src/main/java/ru/otus/library/repositories/BookWithCommentsRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.library.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.otus.library.models.BookWithComments;
-
-public interface BookWithCommentsRepository extends CrudRepository<BookWithComments, Long> {
-    BookWithComments findById(long Id);
-}
--- homework7/librarySpringData/src/main/java/ru/otus/library/service/IOService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.library.service;
-
-public interface IOService {
-    void out(String message);
-
-    String readString();
-}
